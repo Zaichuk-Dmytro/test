09ccebc41be111e804c9d5e00836f724c14993e9
Dmytro <zaichuk.dmytro@gmail.com>
2021-06-11 20:27:06 +0300

    catalog for class

--- js/main.js
+++ js/main.js
@@ -1,2 +1,9 @@
 'use strict';
+import PhonePage from './phones/phones-page.js'
+
+
+
+let phonesPage = new PhonePage({
+    element: document.querySelector('[data-page-container]')
+})
 
---
+++ js/phones/phones-page.js
@@ -0,0 +1,59 @@
+'use strict';
+import PhonesCatalog from './components/phone-catalog.js';
+import PhonesService from './services/phone-services.js';
+
+
+
+
+export default class PhonePage {
+  constructor({element}) {
+    
+    this._element = element;
+    this._render(); 
+
+
+    this._catalog = new PhonesCatalog ({
+      element: this._element.querySelector('[data-phone-catalog]'),
+        phones: PhonesService.getAll(),
+    })
+
+  }
+
+  _render() {
+    this._element.innerHTML = `
+      <div class="row">
+      <div class="col-md-2">
+        <section>
+          <p>
+            Search:
+            <input type="text" />
+          </p>
+
+          <p>
+            Sort by:
+            <select>
+              <option value="name">Alphabetical</option>
+              <option value="age">Newest</option>
+            </select>
+          </p>
+        </section>
+
+        <section>
+          <p>Shopping Cart</p>
+          <ul>
+            <li>Phone 1</li>
+            <li>Phone 2</li>
+            <li>Phone 3</li>
+          </ul>
+        </section>
+      </div>
+
+      <div class="col-md-10">
+        <div data-phone-catalog>
+
+        </div>
+        
+      </div>
+    </div>`
+  }
+}
