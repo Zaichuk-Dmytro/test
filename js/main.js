76a23119cc2e490c3741ba13a6cb90bc549380fa
Dmytro <zaichuk.dmytro@gmail.com>
2021-06-15 14:30:56 +0300

    Merge branch 'main' into test-two

--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
 'use strict';
-
 import PhonePage from './phones/phones-page.js'
 
 
 
 let phonesPage = new PhonePage({
-    element: document.querySelector('[data-page-container]')
+    element: document.querySelector('[data-page-container]'),
 })
